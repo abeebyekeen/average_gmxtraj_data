47501845037e9896dac769f5275b4e189b7c7b58
Abeeb A. Yekeen <abeeb.yekeen@hotmail.com>
2023-06-20 08:31:45 +0800

    Update average_mdtraj_data.py

--- average_mdtraj_data.py
+++ average_mdtraj_data.py
@@ -9,13 +9,21 @@ input_directory = os.getcwd()
 input_files = [file for file in os.listdir(input_directory) if file.endswith(".xvg")]
 
 # Load data from input files
-print("Loading data from the input files found in the working directoy...\n")
-time.sleep(1)
+print("\n Loading data from input files found in the working directoy...\n")
+time.sleep(2)
 data = []
 for file_name in input_files:
     file_path = os.path.join(input_directory, file_name)
     with open(file_path, "r") as file:
         lines = file.readlines()
+        # Fetch the labels for x and y axes
+        for line in lines:
+            if ("xaxis" in line and "label" in line):
+                xlabel = line
+            if ("yaxis" in line and "label" in line):
+                ylabel = line
+            if "@TYPE" in line:
+                plot_type = line            
         # Skip the header lines
         lines = [line for line in lines if not (line.startswith("#") or line.startswith("@"))]
         # Extract x and y values
@@ -27,41 +35,42 @@ for file_name in input_files:
             y.append(float(values[1]))
         data.append((file_name, y))  # Store file name along with y values
 
-# Calculate mean and standard deviation
-print("Calculating mean and standard deviation...\n")
-time.sleep(1)
+# Calculate mean and dtd-dev
+print(" Calculating mean and standard deviation...\n")
+time.sleep(2)
 mean_data = np.mean([y for _, y in data], axis=0)
 std_data = np.std([y for _, y in data], axis=0)
 
 # Generate new xvg file with averaged data
-print("Writing out the time-average plot...\n")
-time.sleep(1)
+print(" Writing out the time-average plot...\n")
+time.sleep(2)
 output_file_mean = "mean_data.xvg"
 with open(output_file_mean, "w") as file:
     file.write("# Mean Data\n")
+    file.write(f"{xlabel}{ylabel}{plot_type}")
     for i in range(len(x)):
-        file.write(f"{x[i]} {mean_data[i]}\n")
+        file.write(f"{x[i]} {mean_data[i]:.7f}\n")
 
 # Generate new file with time, data, and std dev
-print("Writing out the time-average-stddev lookup file...\n")
-time.sleep(1)
+print(" Writing out the time-average-stddev lookup file...\n")
+time.sleep(2)
 output_file_stats = "data_stats.txt"
 with open(output_file_stats, "w") as file:
-    file.write("Time\Data\tStd-dev\n")
+    file.write("Time\tData\tStd-dev\n")
     for i in range(len(x)):
-        file.write(f"{x[i]}\t{mean_data[i]}\t{std_data[i]}\n")
+        file.write(f"{x[i]}\t{mean_data[i]:.7f}\t{std_data[i]:.7f}\n")
 
 # Generate new file with time, y-axis values, mean, and std dev
-print("Writing out the time-data_values-average-stddev lookup file...\n")
-time.sleep(1)
+print(" Writing out the time-data_values-average-stddev lookup file...\n")
+time.sleep(2)
 output_file_data = "data_values.txt"
 with open(output_file_data, "w") as file:
     file.write("Time\t")
-    file.write("\t".join([file_name for file_name, _ in data]))
-    file.write("\tMean\tStandard Deviation\n")
+    file.write("\t".join([file_name.rstrip(".xvg") for file_name, _ in data]))
+    file.write("\tMean\tStd-dev\n")
     for i in range(len(x)):
         file.write(f"{x[i]}\t")
         file.write("\t".join([str(y[i]) for _, y in data]))  # Write y-axis values
-        file.write(f"\t{mean_data[i]}\t{std_data[i]}\n")  # Write mean and std dev values
+        file.write(f"\t{mean_data[i]:.7f}\t{std_data[i]}:.7f\n")  # Write mean and std dev values
 
-print("Run successfully completed!!!")
+print(" Run successfully completed!!!\n")
